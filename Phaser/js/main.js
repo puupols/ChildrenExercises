b1ce8b189fd1d3a35e772a0b4f22ddae5849acea
Your Name <you@example.com>
2018-01-07 05:15:06 -0800

    program to move box

--- Phaser/js/main.js
+++ Phaser/js/main.js
@@ -11,6 +11,11 @@ var shootEvent;
 var timer;
 var total = 0;
 var direction = 1;
+var startMove;
+var moves = [];
+var runningMove = 0;
+var moveLooper;
+var currentMove = '';
 
 function preload() {
   game.load.image('sky', 'assets/sky.png');
@@ -18,8 +23,13 @@ function preload() {
   game.load.image('star', 'assets/star.png');
   game.load.image('box', 'assets/box.png');
   game.load.image('bullet', 'assets/bullet.png');
+  game.load.image('button', 'assets/button.png');
+  game.load.image('rightButton', 'assets/right.png');
+  game.load.image('leftButton', 'assets/left.png');
+  game.load.image('upButton', 'assets/up.png');
+  game.load.image('downButton', 'assets/down.png');
   game.load.spritesheet('dude', 'assets/mydude.png', 32, 32);
-  game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32)
+  game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);  
 }
 
 function create() {
@@ -50,6 +60,34 @@ function create() {
   player.animations.add('left', [3, 4], 10, true);
   player.animations.add('right', [1, 2], 10, true);
 
+
+  button = game.add.button(500, 500, 'button', pressed, this, 0,0,0);
+  
+  upButton = game.add.button(0, 64, 'upButton', pressedUp, this);
+  downButton = game.add.button(32, 64, 'downButton', pressedDown, this);
+  leftButton = game.add.button(64, 64, 'leftButton', pressedLeft, this);
+  rightButton = game.add.button(98, 64, 'rightButton', pressedRight, this);
+
+  function pressedUp(){
+    moves.push('Up');
+  };
+  
+  function pressedDown(){
+    moves.push('Down');
+  };
+
+  function pressedLeft(){
+    moves.push('Left');
+  };
+
+  function pressedRight(){
+    moves.push('Right');
+  };
+
+  function pressed(){
+    startMove = true;
+  }
+
   stars = game.add.group();
   boxes = game.add.group();
   bullets = game.add.group();
@@ -61,9 +99,8 @@ function create() {
   baddies.enableBody = true;
 
 
-  var box = boxes.create(500, game.world.height - 300, 'box');
-  box.body.gravity.y = 300;
-  box.body.drag.x = 100;
+  box = boxes.create(500, game.world.height - 300, 'box');
+  
 
   for (var i = 0; i < 12; i++) {
     var star = stars.create(i * 70, 0, 'star');
@@ -89,6 +126,9 @@ function create() {
 
   scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
+  movesText = game.add.text(16, 98, moves, {fontSize: '32px', fill:'#000' });
+  currentMovesText = game.add.text(16, 140, 'Current moves', {fontSize: '32px', fill:'#000' });
+
   upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
   downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
   leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -96,6 +136,8 @@ function create() {
   shootKay = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 
+  moveLooper = game.time.create(true);
+  moveLooper.loop(2000, moveBox, this);
 }
 
 function update() {
@@ -157,8 +199,64 @@ function update() {
     timer.stop(false);
   }
 
+  if(startMove){
+    moveLooper.start();
+  }
+
+  movesText.text = moves;
+
+  
 }
 
+function moveBox(){    
+    if(moves[runningMove] == 'Right'){
+      currentMove = currentMove + 'Right,';
+      moverRight();
+    } else if (moves[runningMove] == 'Up'){
+      currentMove = currentMove + 'Up,';
+      moveUp();
+    } else if (moves[runningMove] == 'Down'){
+      currentMove = currentMove + 'Down,';
+      moveDown();
+    } else if (moves[runningMove] == 'Left'){
+      currentMove = currentMove + 'Left,';
+      moveLeft();
+    }
+  currentMovesText.text = currentMove;
+  runningMove = runningMove + 1;
+};
+
+function moverRight() {
+  box.body.velocity.x = 50  ;
+  game.time.events.add(2000, function(){
+    box.body.velocity.x = 0;
+    moveRight = false;
+  }); 
+}
+
+function moveUp() {
+  box.body.velocity.y = -50  ;
+  game.time.events.add(2000, function(){
+    box.body.velocity.y = 0;
+    moveRight = false;
+  }); 
+}
+
+function moveDown() {
+  box.body.velocity.y = 50  ;
+  game.time.events.add(2000, function(){
+    box.body.velocity.y = 0;
+    moveRight = false;
+  }); 
+}
+
+function moveLeft() {
+  box.body.velocity.x = -50  ;
+  game.time.events.add(2000, function(){
+    box.body.velocity.x = 0;
+    moveRight = false;
+  }); 
+}
 
 function shoot() {
   var bullet = bullets.create(player.x + 10, player.y + 10, 'bullet');
